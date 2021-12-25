c1af366e44cdaacbb4f2b45ba44c21f5aada7981
Hagen <hagen@paw.bot>
2021-12-25 14:12:48 +0000

    fix: conflicting constants

--- constants.lua
+++ constants.lua
@@ -5,4 +5,18 @@ Constants = {
   Format = {
     PM = '^*^7[^3%s ^5=> ^3%s^7]'
   },
+  DoorIndex = {
+    ['driver'] = 1,
+    ['left'] = 1,
+    ['passenger'] = 2,
+    ['right'] = 2,
+    ['rear left passenger'] = 3,
+    ['rear left'] = 3,
+    ['rear right passenger'] = 4,
+    ['rear right'] = 4,
+    ['hood'] = 5,
+    ['bonnet'] = 5,
+    ['trunk'] = 6,
+    ['boot'] = 6,
+  },
 }
